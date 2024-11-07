0b83b20df7165120d4e254e7fa982af92c869b72
Jackson Horan <jacksonhoran1@gmail.com>
2024-11-07 09:07:49 -0600

    update psuedocode in Main and MathProblem. Created Static Final variable for user difficulty

--- Main/Main.java
+++ Main/Main.java
@@ -5,11 +5,11 @@ public class Main{
         MathProblem.getMathprobem(difficulty); // gets a math problem given the isers chosen difficulty
     }
     public static int getDifficulty(){
-        Scanner console = new Scanner();
+        Scanner console = new Scanner(System.in);
         System.out.println("Choose your difficulty by typing the number corresponding to difficulty level.")
-        System.out.println("1 - Easy")
-        System.out.println("2 - Medium")
-        System.out.println("3 - Hard")
+        System.out.println("1 - Easy");
+        System.out.println("2 - Medium");
+        System.out.println("3 - Hard");
         int difficulty  = console.nextInt();
         return difficulty;
     }
