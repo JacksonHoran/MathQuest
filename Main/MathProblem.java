4513d1bfeef30b794545d116ff8a8a517b9f778f
Jackson Horan <jacksonhoran1@gmail.com>
2024-11-18 18:09:39 -0600

    final changes to MathProblem

--- Main/MathProblem.java
+++ Main/MathProblem.java
@@ -1,100 +1,130 @@
 import java.util.Random;
 import java.util.Scanner;
 
+public class MathProblem {
 
-public class MathProblem{
-   
-    /* makes a math problem with randomized operators and integer values
-     * this will use the random library to randomly choose the order of the operators 
-     * ,the numbers in the expression and the type of the math problem. there will be 
-     * for example three or four differnt style maht problems. all of these three methods
-     *  will work the same except that they will each create problems of different difficulty
+    /* makes a math problem with randomized operators and integer values,
+     * uses the random library to randomly choose the integers and uses a random boolean 
+     * to decide which combinations of operators to use. the method uses a ternary
+     *  operator instead of an if/else statement to decide which equations skeleton to use
+     * the method returns true or false based on whether the user's answer is correct or incorrect.
      */
-    public static boolean createMathProblem_Dif1(){
-       Random random = new Random();
-       Scanner input = new Scanner(System.in);
-
-       int num1 = random.nextInt(10)+1;
-       int num2 = random.nextInt(10)+1;
-       char operators = random.nextBoolean() ? '+': '-';
-
-       int result  = operators == '+' ? num1+num2: num1-num2;
-       if (operators == '+'){
-        System.out.println("SOLVE THIS PROBLEM TO GET PAST THE MONSTER!");
-        System.out.println(num1 + "+" + num2);
-       }else{
-        System.out.println("SOLVE THIS PROBLEM TO GET PAST THE MONSTER!");
-        System.out.println(num1 + "+" + num2);
-       }
-    
-       int userAnswer = input.nextInt();
-       return userAnswer == result;
-    }
-
-    public static boolean createMathProblem_Dif2(){ 
+    public static boolean createMathProblem_Dif1() {
         Random random = new Random();
         Scanner input = new Scanner(System.in);
 
+        // gets all the random info
+        int num1 = random.nextInt(10) + 1;
+        int num2 = random.nextInt(10) + 1;
+        char operators = random.nextBoolean() ? '+' : '-';
 
-        int num1 = random.nextInt(10)+1;
-        int num2 = random.nextInt(10)+1;
-        int num3 = random.nextInt(50)+1;
-        char operators = random.nextBoolean() ? '*': '+';
+        // chooses the equation format
+        int result = operators == '+' ? num1 + num2 : num1 - num2;
 
-        int result  = operators == '*' ? (num1*num2)+num3: num1+num2+num3;
-
-        if(operators == '*'){
-            System.out.println("SOLVE THIS PROBLEM TO GET PAST THE MONSTER!");
-            System.out.println( "(" + num1 + "*" + num2 + ")" + "+" + num3);
-        }else{
-            System.out.println("SOLVE THIS PROBLEM TO GET PAST THE MONSTER!");
-            System.out.println(num1 + "+" + num2 + "+" + num3);
+        // shows the user the equation
+        System.out.println("SOLVE THIS PROBLEM TO GET PAST THE MONSTER!");
+        if (operators == '+') {
+            System.out.println(num1 + " + " + num2);
+        } else {
+            System.out.println(num1 + " - " + num2);
         }
+
+        // returns whether user is correct or not
         int userAnswer = input.nextInt();
-        return result == userAnswer;
+        return userAnswer == result;
     }
 
-    public static boolean createMathProblem_Dif3(){
+    /* makes a math problem with randomized operators and integer values,
+     * uses the random library to randomly choose the integers and uses a random boolean 
+     * to decide which combinations of operators to use. the method uses a ternary
+     *  operator instead of an if/else statement to decide which equations skeleton to use
+     * the method returns true or false based on whether the user's answer is correct or incorrect.
+     */
+    public static boolean createMathProblem_Dif2() {
         Random random = new Random();
-        Scanner input = new Scanenr();
+        Scanner input = new Scanner(System.in);
 
-        int num1 = random.nextInt(12)+1;
-        int num2 = random.nextInt(12)+1;
-        int num3 = random.nextInt(12)+1;
-        int num4 = random.nextInt(99)+1;
-        char operators = random.nextBoolean() ? '*': '+';
+        // gets all random info
+        int num1 = random.nextInt(10) + 1;
+        int num2 = random.nextInt(10) + 1;
+        int num3 = random.nextInt(50) + 1;
+        char operators = random.nextBoolean() ? '*' : '+';
 
-        int result  = operators == '*' ? ((num1*num2)/num3)+num4: ((num1/num2)*num3)+num4;
+        // chooses equation skeleton
+        int result = operators == '*' ? (num1 * num2) + num3 : num1 + num2 + num3;
 
-        if(operators == '*'){
-            System.out.println("SOLVE THIS PROBLEM TO GET PAST THE MONSTER!");
-            System.out.println("(" + "(" + num1 + "*" + num2 + ")" + "/" + num3 + ")" + "+" + num4);
-        }else{
-            System.out.println("SOLVE THIS PROBLEM TO GET PAST THE MONSTER!");
-            System.out.println("(" + "(" + num1 + "/" + num2 + ")" + "*" + num3 + ")" + "+" + num4);
+        // shows user the equation
+        System.out.println("SOLVE THIS PROBLEM TO GET PAST THE MONSTER!");
+        if (operators == '*') {
+            System.out.println("(" + num1 + " * " + num2 + ")" + " + " + num3);
+        } else {
+            System.out.println(num1 + " + " + num2 + " + " + num3);
         }
+
+        // returns whether user is correct or incorrect
         int userAnswer = input.nextInt();
         return result == userAnswer;
     }
 
+    /* makes a math problem with randomized operators and integer values,
+     * uses the random library to randomly choose the integers and uses a random boolean 
+     * to decide which combinations of operators to use. the method uses a ternary
+     *  operator instead of an if/else statement to decide which equations skeleton to use
+     * the method returns true or false based on whether the user's answer is correct or incorrect.
+     */
+    public static boolean createMathProblem_Dif3() {
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
 
-    /*this method will call the correct difficulty math problem method based 
-    on the users input. This method takes the users chosen difficulty as a parameter 
-    and prints the math problem to the console. */
-    public static void getMathprobem(int difficulty){
+        // gets random info
+        int num1 = random.nextInt(12) + 1;
+        int num2 = random.nextInt(12) + 1;
+        int num3 = random.nextInt(12) + 1;
+        int num4 = random.nextInt(99) + 1;
+        char operators = random.nextBoolean() ? '*' : '+';
+
+        // chooses equation format
+        int result = operators == '*' ? ((num1 * num2) / num3) + num4 : ((num1 / num2) * num3) + num4;
+
+        // shows user the equation
+        System.out.println("SOLVE THIS PROBLEM TO GET PAST THE MONSTER!");
+        if (operators == '*') {
+            System.out.println("(" + "(" + num1 + " * " + num2 + ")" + " / " + num3 + ")" + " + " + num4);
+        } else {
+            System.out.println("(" + "(" + num1 + " / " + num2 + ")" + " * " + num3 + ")" + " + " + num4);
+        }
+
+        // returns whether or not the user is correct
+        int userAnswer = input.nextInt();
+        return result == userAnswer;
+    }
+
+    /* this method will call the correct difficulty math problem method based 
+     * on the users input. This method takes the users chosen difficulty as a parameter 
+     * and prints the math problem to the console. */
+    public static void getMathProblem(int difficulty) {
+        boolean isCorrect;
+
         switch (difficulty) {
             case 1:
-                createMathProblem_Dif1();
+                isCorrect = createMathProblem_Dif1();
                 break;
             case 2:
-                createMathProblem_Dif2();
+                isCorrect = createMathProblem_Dif2();
                 break;
             case 3:
-                createMathProblem_Dif3();
+                isCorrect = createMathProblem_Dif3();
                 break;
             default:
                 System.out.println("Invalid difficulty level. Choose 1, 2, or 3.");
-                break;
+                return;
+        }
+
+        // informs the user whether they were correct or not
+        if (isCorrect) {
+            System.out.println("Correct! You defeated the monster!");
+        } else {
+            System.out.println("Incorrect! The monster defeated you!");
         }
     }
 }
