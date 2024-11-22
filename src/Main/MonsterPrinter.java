61c7a86a6ec8360874c162cae2d3f62102486d55
Jackson Horan <jacksonhoran1@gmail.com>
2024-11-22 15:35:38 -0600

    making monsters

---
+++ src/Main/MonsterPrinter.java
@@ -0,0 +1,124 @@
+import java.util.Random;
+
+public class MonsterPrinter {
+
+    public static void printRandomMonster() {
+        Random random = new Random();
+
+        int num = random.nextInt(10) + 1;
+        if (num == 1) {
+            monster1();
+        } else if (num == 2) {
+            monster2();
+        } else if (num == 3) {
+            monster3();
+        } else if (num == 4) {
+            monster4();
+        } else if (num == 5) {
+            monster5();
+        } else if (num == 6) {
+            monster6();
+        } else if (num == 7) {
+            monster7();
+        } else if (num == 8) {
+            monster8();
+        } else if (num == 9) {
+            monster9();
+        } else {
+            monster10();
+        }
+    }
+
+    public static void monster1() {
+        System.out.println("                  __,-----,,,,  ,,,--------,__ ");
+        System.out.println("                _-/|\\\\/|\\\\/|\\\\\\\\|/\\\\\\//|/|//|\\\\_ ");
+        System.out.println("               /|\\/\\//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ ");
+        System.out.println("             //|//           \\\\\\\\/            |\\\\|\\ ");
+        System.out.println("            ///|\\/             \\/               \\|\\|\\ ");
+        System.out.println("           |/|//                                 |\\\\|\\  ");
+        System.out.println("          |/|/                                    \\|\\|\\ ");
+        System.out.println("          ///;    ,,=====,,,  ~~-~~  ,,,=====,,    ;|\\|\\ ");
+        System.out.println("         |/|/   '\"          `'     '\"          \"'   ;|\\| ");
+        System.out.println("         ||/`;   _--~~~~--__         __--~~~~--_   ;/|\\| ");
+        System.out.println("         /|||;  :  /       \\~~-___-~~/       \\  :  ;|\\| ");
+        System.out.println("         /\\|;    -_\\  (o)  / ,'; ;', \\  (o)  /_-    ;|| ");
+        System.out.println("         |\\|;      ~-____--~'  ; ;  '~--____-~      ;\\| ");
+        System.out.println("          ||;            ,`   ;   ;   ',            ;|| ");
+        System.out.println("        __|\\ ;        ,'`    (  _  )    `',        ;/|__ ");
+        System.out.println("    _,-~   \\|/;    ,'`        ~~ ~~        `',    ;|\\   ~-,_ ");
+        System.out.println("  ,'         ||;  '                           '  ;\\|/       `, ");
+        System.out.println(" , _          ; ,         _--~~-~~--_           ;            _',");
+        System.out.println(",-' `;-,        ;        ,; |_| | |_| ;,       ;;        ,-;' `-,");
+        System.out.println("      ; `,      ;       ;_| : `~'~' : |_;       ;      ,' ; ");
+        System.out.println("       ;  `,     ;     :  `\\/       \\/   :     ;     ,'  ; ");
+        System.out.println("        ;   `,    ;     :               ;     ;    ,'   ; ");
+        System.out.println("         ;    `,_  ;     ;./\\_     /\\.;     ;   _,    ; ");
+        System.out.println("      _-'        ;  ;     ~~--|~|~|--~~     ;   ;       '-_ ");
+        System.out.println("  _,-'            ;  ;        ~~~~~        ;   ;           `-,_ ");
+        System.out.println(",~                 ;  \\`~--__         __--~/  ;                ~,");
+        System.out.println("                    ;   \\   ~~-----~~    /   ;                   ");
+        System.out.println("                     ~-_  \\  /  |  \\   /  _-~                    ");
+        System.out.println("                        ~~-\\/   |   \\/ -~~                       ");
+        System.out.println("                       (=)=;==========;=(=) ");
+    }
+
+    public static void monster2() {
+        System.out.println("             ___          ");
+        System.out.println("            /   \\        ");
+        System.out.println("       /\\  | . . \\       ");
+        System.out.println("     ////\\ |     ||       ");
+        System.out.println("   ////   \\ ___//\\       ");
+        System.out.println("  ///      \\      \\      ");
+        System.out.println(" ///       |\\      |     ");
+        System.out.println("//         | \\\\  \\   \\    ");
+        System.out.println("/          |  \\\\  \\   \\   ");
+        System.out.println("           |   \\\\ /   /   ");
+        System.out.println("           |    \\/   /    ");
+        System.out.println("           |     \\\\/|     ");
+        System.out.println("           |      \\\\|     ");
+        System.out.println("           |       \\\\     ");
+        System.out.println("           |        |     ");
+        System.out.println("           |_________\\    ");
+    }
+
+    public static void monster3() {
+        System.out.println("               __.......__");
+        System.out.println("            .-:::::::::::::-.");
+        System.out.println("          .:::''':::::::''':::.");
+        System.out.println("        .:::'     `:::'     `:::.");
+        System.out.println("   .'\\  ::'   ^^^  `:'  ^^^   '::  /`.");
+        System.out.println("  :   \\ ::   _.__       __._   :: /   ;");
+        System.out.println(" :     \\`: .' ___\\     /___ `. :'/     ;");
+        System.out.println(":       /\\   (_|_)\\   /(_|_)   /\\       ;");
+        System.out.println(":      / .\\   __.' ) ( `.__   /. \\      ;");
+        System.out.println(":      \\ (        {   }        ) /      ;");
+        System.out.println(" :      `-(     .  ^\"^  .     )-'      ;");
+        System.out.println("  `.       \\  .'<`-._.-'>'.  /       .'");
+        System.out.println("    `.      \\    \\;`.';/    /      .'");
+        System.out.println("      `._    `-._       _.-'    _.'");
+        System.out.println("       .'`-.__ .'`-._.-'`. __.-'`.");
+        System.out.println("     .'       `.         .'       `.");
+        System.out.println("   .'           `-.   .-'           `.");
+    }
+
+    public static void monster4() {
+    }
+
+    public static void monster5() {
+    }
+
+    public static void monster6() {
+    }
+
+    public static void monster7() {
+    }
+
+    public static void monster8() {
+    }
+
+    public static void monster9() {
+    }
+
+    public static void monster10() {
+    }
+}
