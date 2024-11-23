e11022a13275c6256b42fd749d68fb0a352a8527
janedbar <jdunbar@luc.edu>
2024-11-23 17:24:26 -0600

    Update Ending.java
    got rid of parameters for showEnding, changed totalPoints to enemyencounter.points

--- Ending.java
+++ Ending.java
@@ -2,11 +2,12 @@
 public class Ending {
 
   public static boolean restartGame;
+  
 
-  public static void showEnding(int totalPoints) {
+  public static void showEnding() {
     String endingMessage;
-    String userScore = PreGameInfo.userName + ", you scored " + totalPoints + " total points!";
-    if (totalPoints <= 12) {
+    String userScore = PreGameInfo.userName + ", you scored " + EnemyEncounter.points + " total points!";
+    if (EnemyEncounter.points <= 12) {
       endingMessage = "You find yourself here, home at last!! However, all is quiet..."
           + "\nIt appears nobody is around…. Where could they be??"
           + "\nYou hear a commotion come from behind one of your neighbors homes…."
@@ -15,7 +16,7 @@ public class Ending {
           + "\neveryone you know!! As the creature approaches you, teeth bared, it growls. Maybe if you "
           + "\nwere a little better at math, this wouldn’t have happened… You close your eyes, realizing he’s "
           + "\nright, and accept your fate.";
-    } else if (totalPoints <= 15) {
+    } else if (EnemyEncounter.points <= 15) {
       endingMessage = "You excitedly rush towards your village, relieved to be home."
           + "\nYou rush to your house, and find your mother washing dishes in the kitchen. "
           + "\nShe turns around and greets you, nonchalantly as if nothing happened and you didn’t go missing. "
@@ -41,7 +42,7 @@ public class Ending {
     System.out.println();
     System.out.println(userScore);
     System.out.println();
-    PreGameInfo.input.nextLine();
+    
 
     while (true) {
       System.out.println("Would you like to restart the game? Type Y or N.");
