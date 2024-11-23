46fe88d4104e25ebccff755a9625a128b4ec64e4
janedbar <jdunbar@luc.edu>
2024-11-23 13:15:42 -0600

    Create ShowMonster.java

---
+++ src/Main/ShowMonster.java
@@ -0,0 +1,11 @@
+import java.util.Random; //standin
+
+public class ShowMonster{
+
+  Random generator = new Random();
+  int randomIndex = generator.nextInt(monsterList.length);
+  return monsterList[randomIndex];
+
+  public static boolean chooseMonster(int difficulty){
+    
+        }
