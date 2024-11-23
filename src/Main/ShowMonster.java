33f8792a2a4b0df2b43da823f2e41706d10bfa27
janedbar <jdunbar@luc.edu>
2024-11-23 13:17:07 -0600

    Update ShowMonster.java
    oops

--- src/Main/ShowMonster.java
+++ src/Main/ShowMonster.java
@@ -1,11 +1,1 @@
-import java.util.Random; //standin
 
-public class ShowMonster{
-
-  Random generator = new Random();
-  int randomIndex = generator.nextInt(monsterList.length);
-  return monsterList[randomIndex];
-
-  public static boolean chooseMonster(int difficulty){
-    
-        }
