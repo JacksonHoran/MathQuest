2ae8d493401a4460ab2746606a70bbfc59f818ed
Jackson Horan <159923763+JacksonHoran@users.noreply.github.com>
2024-11-24 15:13:10 -0600

    Update EnemyEncounter.java
    psuedocode added

--- EnemyEncounter.java
+++ EnemyEncounter.java
@@ -11,14 +11,16 @@ public class EnemyEncounter {
     1 life for easy mode, 2 for medium and 3 lives for hard mode
     */
     public static void livesCounterInit() {
-        if (PreGameInfo.difficulty == 1) { //if user selects easy mode
+        //if user selects easy mode
+        if (PreGameInfo.difficulty == 1) { 
             livesCounter = 1; //add one to life counter
-        } else if (PreGameInfo.difficulty == 2) { //if user selects medium mode
+        //if user selects medium mode
+        } else if (PreGameInfo.difficulty == 2) { 
             livesCounter = 2; //add two to life counter
-        } else { //if user selects hard mode
+        //if user selects hard mode
+        } else { 
             livesCounter = 3; //add three to life counter
         }
-
     }
 
     /*
@@ -56,9 +58,12 @@ public class EnemyEncounter {
                 calculatePoints(); // Adds points based on number of attempts
                 correctAnswer = true;
                 Main.pathsTraversed += 1;
+            // if wrong and max attemps reached
             } else if (attemptCounter == 4) {
+                // increent pathsTraversed by one
                 System.out.println("Incorrect! The Monster defeated you!");
                 Main.pathsTraversed += 1;
+            // if wrong but attemps less than 4
             } else {
                 System.out.println("Incorrect!");
                 System.out.println();
@@ -71,31 +76,49 @@ public class EnemyEncounter {
         }
     }
 
+
+    /*
+    this method helps implement a "life" system.
+    when this method is called the static varible named livesCounter is decremented by one
+
+    next the method checks if the user has run out of lvies by using an if/else statement.
+    if the user has run out of lives, the game is ended by setting pathsTeaversed to 100 which 
+    will end the while that runs that runs the game portion of the program
+
+    if the user still has at least one life left, they are given a mesage letting them know that 
+    they were beat by the monster and they have x amount of lives left.
+    */
+    
     private static void loseLife() {
-        livesCounter--; // Deduct a life
+        livesCounter--; // decrement a life
 
         if (livesCounter <= 0) {
-            // Game over if no lives remain
+            // game over if no lives remain
             System.out.println();
             System.out.println("Game over, you've lost all your lives.");
             Main.pathsTraversed = 100;
         } else {
-            // Print remaining lives if any are left
+            // print remaining lives if any are left
             System.out.println("The monster beat you, you have " + livesCounter + " lives left.");
-            // Allow the game to continue by not changing the runGame flag
         }
     }
 
+    /*
+    This is a helper method to add to the users points total when they answer a 
+    math question correctly.
 
+    this uses chain of if/else statements to add points to the user game total 
+    depending on how many attempts they have used.
+    */
     private static void calculatePoints() {
-        if (attemptCounter == 1) {
-            points += 4;
-        } else if (attemptCounter == 2) {
-            points += 3;
-        } else if (attemptCounter == 3) {
-            points += 2;
-        } else {
-            points += 1;
+        if (attemptCounter == 1) { // one attempt
+            points += 4; //add 4
+        } else if (attemptCounter == 2) { // two attempts
+           points += 3; // add 3
+        } else if (attemptCounter == 3) { // three attemps
+            points += 2; // add 2 
+        } else { // four attempts
+            points += 1; //add 1
         }
     }
 }
