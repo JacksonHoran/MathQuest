09292a1c7a02a3949266914b8f67196f090e7a89
Jackson Horan <159923763+JacksonHoran@users.noreply.github.com>
2024-11-24 15:29:19 -0600

    Update Main.java
    psuedocode

--- Main.java
+++ Main.java
@@ -1,43 +1,48 @@
 
 public class Main {
     public static void main(String[] args) {
+        //initialozes runGame Boolean
         PreGameInfo.runGameInit();
+
+        // runs till runGame is set to false
         while (PreGameInfo.runGame) {
-            PreGameInfo.getUsername();
-            PreGameInfo.getDifficulty();
-            System.out.println(PreGameInfo.difficulty);
-            EnemyEncounter.livesCounterInit();
-            game(PreGameInfo.difficulty);
+            PreGameInfo.getUsername(); // gets a username
+            PreGameInfo.getDifficulty(); // gets users desired difficulty level
+            EnemyEncounter.livesCounterInit(); // initializes the how many lives the player gets
+            game(PreGameInfo.difficulty); // calls the methods that progress the game
             break;
         }
     }
 
+    
+    // initializes an integer to track how many times the player chosen a path
     public static int pathsTraversed = 0;
-
+    
+    /*
+    this is the game driver method, the method first prints some narration 
+    to the player, then calls the choose path method so long as the number
+    of paths traversed is less than the total number of allowed paths.
+    */
     private static void game(int difficulty) {
-        do {
-            // Reset game state before starting a new game
-            EnemyEncounter.points = 0; // Reset points
-            PreGameInfo.runGame = true; // Ensure the game is set to run
+        
+        // Print narration
+        System.out.println(
+                "It seems you've found yourself in the middle of the forest, quite a ways from home. "
+                        + "You're not sure where you are or how you got here, but you're determined to find your way back.");
+        System.out.println("Prepare yourself! The adventure begins...");
 
-            System.out.println(
-                    "It seems you've found yourself in the middle of the forest, quite a ways from home. "
-                            + "You're not sure where you are or how you got here, but you're determined to find your way back.");
-            System.out.println("Prepare yourself! The adventure begins...");
+        // total number of paths to play is set by the difficulty plus 2
+        int numOfPaths = difficulty + 2;
 
-            int numOfPaths = difficulty + 2;
-
-
-            while (true) {
-                if (numOfPaths > pathsTraversed) {
-                    Path.choosePath(difficulty);
-                } else {
-                    Ending.showEnding();
-                    break;
-                }
+        // calls choosePath() till player reaches max allowed
+        while (true) {
+            if (numOfPaths > pathsTraversed) {
+                Path.choosePath(difficulty);
+            } else {
+                Ending.showEnding();
+                break;
             }
-        } while (Ending.restartGame); // Restart the game if the player chooses to replay
-
+        }
         System.out.println("Thank you for playing!!!");
     }
 }
